9a84930d43884c5656c606f865b9cd83bcb91bf4
AlyasBrix <baring.keitzbryelle@gmail.com>
2026-02-27 23:58:58 +0800

    edit and delete added

--- project/baring/urls.py
+++ project/baring/urls.py
@@ -1,8 +1,24 @@
 from django.urls import path
-from .views import EmployeeListCreateView, DepartmentListCreateView, ProjectListCreateView
+from .views import (
+    EmployeeListCreateView,
+    EmployeeRetrieveUpdateDestroyView,
+    DepartmentListCreateView,
+    DepartmentRetrieveUpdateDestroyView,
+    ProjectListCreateView,
+    ProjectRetrieveUpdateDestroyView,
+)
 
 urlpatterns = [
-    path('employees/', EmployeeListCreateView.as_view(), name='employees'),
-    path('departments/', DepartmentListCreateView.as_view(), name='departments'),
-    path('projects/', ProjectListCreateView.as_view(), name='projects'),
+
+    # EMPLOYEE
+    path('employees/', EmployeeListCreateView.as_view(), name='employee-list-create'),
+    path('employees/<int:pk>/', EmployeeRetrieveUpdateDestroyView.as_view(), name='employee-detail'),
+
+    # DEPARTMENT
+    path('departments/', DepartmentListCreateView.as_view(), name='department-list-create'),
+    path('departments/<int:pk>/', DepartmentRetrieveUpdateDestroyView.as_view(), name='department-detail'),
+
+    # PROJECT
+    path('projects/', ProjectListCreateView.as_view(), name='project-list-create'),
+    path('projects/<int:pk>/', ProjectRetrieveUpdateDestroyView.as_view(), name='project-detail'),
 ]
--- project/baring/views.py
+++ project/baring/views.py
@@ -1,10 +1,15 @@
-from rest_framework.generics import ListCreateAPIView
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
 from .models import Employee, Department, Project
 from .serializers import EmployeeSerializer, DepartmentSerializer, ProjectSerializer
 
-# Create your views here.
+
 
 class EmployeeListCreateView(ListCreateAPIView):
+    queryset = Employee.objects.all()
+    serializer_class = EmployeeSerializer
+
+
+class EmployeeRetrieveUpdateDestroyView(RetrieveUpdateDestroyAPIView):
     queryset = Employee.objects.all()
     serializer_class = EmployeeSerializer
 
@@ -14,6 +19,16 @@ class DepartmentListCreateView(ListCreateAPIView):
     serializer_class = DepartmentSerializer
 
 
+class DepartmentRetrieveUpdateDestroyView(RetrieveUpdateDestroyAPIView):
+    queryset = Department.objects.all()
+    serializer_class = DepartmentSerializer
+
+
 class ProjectListCreateView(ListCreateAPIView):
+    queryset = Project.objects.all()
+    serializer_class = ProjectSerializer
+
+
+class ProjectRetrieveUpdateDestroyView(RetrieveUpdateDestroyAPIView):
     queryset = Project.objects.all()
     serializer_class = ProjectSerializer
